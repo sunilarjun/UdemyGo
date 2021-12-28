6ec5ac85b6edc18a054ddcc0de8ac4373dc7d975
sunilarjun <sunil@circleci.com>
2021-12-28 12:06:15 -0800

    slices and arrays

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//var card string = "Ace of Spades"
+	//above is long form and explicit, below does the same
+	//go can infer types to a degree, only use := syntax when intially defining a var
+
+	//declare a slice, so we can have a number of cards
+	cards := []string{"Ace of Spades", newCard()}
+	//append does not modify original slice, but instead appends and creates a new slice
+	cards = append(cards, "Six of Spades")
+
+	//i is index of card in the slice, here is a slice, take the slice and iterate over every single record in the slice
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
+
+func newCard() string {
+	return "Five of Diamonds"
+}
+
+//Go is a statically typed language, so variables have an assigned type always
+//Basic data types: bool, string, int, float64
+//DS in Go:
+//Array: Fixed length list of things, very fixed length of records, items inside must have identical type
+//Slice: An array that can grow or shrink, items inside must have identical type

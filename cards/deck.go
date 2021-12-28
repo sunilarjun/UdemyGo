e54f662569947e30f01cc8ee583e1732dffa51f6
sunilarjun <sunil@circleci.com>
2021-12-28 13:23:53 -0800

    added func deal and newdeck, as well as concepts of returning multiple vals

--- cards/deck.go
+++ cards/deck.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
 //Create a new type of deck which is
 // a slice of strings
 type deck []string
+
+//no receiver because it is a new creation, nothing to throw a method on
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+//any var of type deck in our func now gets access to the print method
+// (d deck) is a receiver, by convention single or double letter of type
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
--- cards/main.go
+++ cards/main.go
@@ -1,26 +1,31 @@
 package main
 
-import "fmt"
-
 func main() {
 	//var card string = "Ace of Spades"
 	//above is long form and explicit, below does the same
 	//go can infer types to a degree, only use := syntax when intially defining a var
 
 	//declare a slice, so we can have a number of cards
-	cards := deck{"Ace of Spades", newCard()}
+	//cards := deck{"Ace of Diamonds", newCard()}
 	//append does not modify original slice, but instead appends and creates a new slice
-	cards = append(cards, "Six of Spades")
+	//cards = append(cards, "Six of Spades")
 
 	//i is index of card in the slice, here is a slice, take the slice and iterate over every single record in the slice
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	//for i, card := range cards {
+	//	fmt.Println(i, card)
+	//}
+
+	cards := newDeck()
+
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
 }
 
-func newCard() string {
-	return "Five of Diamonds"
-}
+//func newCard() string {
+//	return "Five of Diamonds"
+//}
 
 //Go is a statically typed language, so variables have an assigned type always
 //Basic data types: bool, string, int, float64

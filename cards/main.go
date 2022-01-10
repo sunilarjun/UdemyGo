4f3532a941b1543f8bebfa7b6646a2df9c4624f6
sunilarjun <sunil@circleci.com>
2022-01-10 15:50:09 -0800

    adding shuffle function to rand cards

--- cards/main.go
+++ cards/main.go
@@ -18,7 +18,11 @@ func main() {
 	//cards := newDeck()
 	//cards.saveToFile("my_cards")
 
-	cards := newDeckFromFile("my_cards")
+	//cards := newDeckFromFile("my_cards")
+	//cards.print()
+
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 
 }

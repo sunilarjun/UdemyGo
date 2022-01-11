32a2532afc3fa47ec0b2583e3925f92f93e87c27
sunilarjun <sunil@circleci.com>
2022-01-11 15:45:02 -0800

    add evenodd assignment

---
+++ cards/evenodd.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func evenodd() {
+	fmt.Print("Enter number : ")
+
+	var n int
+
+	fmt.Scanln(&n)
+	/*  Conditional Statement if .... else ........     */
+	if n%2 == 0 {
+		fmt.Println(n, "is Even number")
+	} else {
+		fmt.Println(n, "is Odd number")
+	}
+}

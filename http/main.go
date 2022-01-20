9cf3e616685b5d053e3093b1966992689f0687ce
sunilarjun <sunil@circleci.com>
2022-01-20 15:24:31 -0800

    intial response func

--- http/main.go
+++ http/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Print(resp)
 }

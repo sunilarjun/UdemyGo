b93700655572848e667739fd569da211af7189f4
sunilarjun <sunil@circleci.com>
2022-01-20 14:39:17 -0800

    finished mapping, starting interfaces

--- map/main.go
+++ map/main.go
@@ -7,17 +7,26 @@ func main() {
 	//var colors map[string]string
 
 	//option 2
-	colors := make(map[int]string)
+	//colors := make(map[int]string)
 
 	//option 3
-	//colors := map[string]string{
-	//	"red":   "#ff0000",
-	//	"green": "#00ff00",
-	//}
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"white": "#ffffff",
+	}
 
-	colors[10] = "#ffffff"
+	//colors[10] = "#ffffff"
 
-	delete(colors, 10)
+	//delete(colors, 10)
 
-	fmt.Println(colors)
+	//fmt.Println(colors)
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }

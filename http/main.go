08ceec5b8b4469de3a0a4822300474f7210641ab
sunilarjun <sunil@circleci.com>
2022-01-20 16:35:12 -0800

    logwriter function and doc dive

--- http/main.go
+++ http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -14,7 +15,17 @@ func main() {
 	}
 
 	//make a byte slice init w 99999 empty elements
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
+	//bs := make([]byte, 99999)
+	//resp.Body.Read(bs)
+	//fmt.Println(string(bs))
+
+	lw := logWriter{}
+	//now to do it in one go....lol
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println((string(bs)))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
